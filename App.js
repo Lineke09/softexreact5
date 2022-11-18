bdf4db1d34cbe085ba15ebefde8e2051eacb5f0d
Lineke09 <109102261+Lineke09@users.noreply.github.com>
2022-11-18 07:22:34 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 
+import Pessoa from './components/Pessoa'
 import Empresa from "./Pages/Empresa";
 import React from "react";
 import Home from "./Pages/Home";
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-function App() {
+import { useState, useEffect } from "react"
+
+
+export default function App() {
+  const [numero, setNumero] = useState(0)
+  const [nome, setNome] = useState('João')
+
+  useEffect(() => {
+    document.title ="'Você clicou " + numero  + " vezes"
+
+  },[numero])
+ 
 
   return (
     <div className="App">
       <header className="App-header">
-
+      <Pessoa/>
+      <h1> Matheus Lineke</h1>
+      <button onClick={() => setNumero(item => item + 1)}> Aumentar </button>
+      <br/><br/>
+      <h2>Numero {numero} </h2>
+      <h2> Olá {nome} </h2>
+      <button onClick={() => setNome("Matheus") } > Mudar Nome</button>
+    
     
       <Router>
         <ul>
@@ -26,7 +45,7 @@ function App() {
       </Router>
       </header>
     </div>
+    
   );
 }
  
-export default App;
